f1c2715c1c1d2ffebabd7bd4fe6a8b28d012728d
sreekutten <sreekuttenps5@gmail.com>
2024-12-13 21:49:45 +0530

    homepage

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,14 +1,58 @@
 "use client"
+import { useState } from "react"
+import HomeCard from "./HomeCard"
+import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = 
+  useState <'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+  const createMeeting = () => {
+
+  }
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+
+      <HomeCard 
+        img = "/icons/add-meeting.svg"
+        title = "New Meeting"
+        description = "Start an instant meeting"
+        handleClick = {() => setMeetingState ('isInstantMeeting')}
+        className = "bg-orange-1"
+      />
+      <HomeCard 
+        img = "/icons/schedule.svg"
+        title = "Schedule Meeting"
+        description = "Plan your meeting"
+        handleClick = {() => setMeetingState ('isScheduleMeeting')}
+        className = "bg-blue-1"
+      />
       
-      <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[18px] cursor-pointer'
-        onClick={() => {}}>
-        Box1
-      </div>
-      
+      <HomeCard 
+        img = "/icons/join-meeting.svg"
+        title = "Join Meeting"
+        description = "Via invitation link"
+        handleClick = {() => setMeetingState ('isJoiningMeeting')}
+        className = "bg-yellow-1"
+      />
+      <HomeCard 
+        img = "/icons/recordings.svg"
+        title = "View Recordings"
+        description = "Check out your recordings"
+        handleClick = {() => router.push('/recordings')}
+        className = "bg-purple-1"
+      />
+
+      <MeetingModal
+        isOpen = {meetingState === 'isInstantMeeting'}
+        onClose ={() => setMeetingState(undefined)}
+        title = "Start an Instant Meeting"
+        className ="text-center"
+        buttonText="Start Meeting"
+        handleClick = {createMeeting}
+      />
+
     </section>
   )
 }
