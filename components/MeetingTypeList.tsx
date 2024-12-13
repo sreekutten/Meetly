7e3b0aaae4aad2724888e37d0de2a5fcfc640b43
sreekutten <sreekuttenps5@gmail.com>
2024-12-13 18:13:48 +0530

    home page

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+const MeetingTypeList = () => {
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+      
+      <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[18px] cursor-pointer'
+        onClick={() => {}}>
+        Box1
+      </div>
+      
+    </section>
+  )
+}
+
+export default MeetingTypeList
